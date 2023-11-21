027a78464c6de53fc326e3a7592e10ac10df8722
Henrique Marques <138395352+LHenrique-Marques@users.noreply.github.com>
2023-11-21 20:02:16 -0300

    Update 2.0
    corrigindo o erro do programa, ele não conseguia validar os outros usuarios, apenas o primeiro da lista..

--- sistema-login.py
+++ sistema-login.py
@@ -3,36 +3,44 @@ logado = False
 while not logado:
     try:
         escolha = int(input("""
-        Menu:
-        
-        1 - Logar
-        2 - Cadastrar
+            Menu:
+            
+            1 - Logar
+            2 - Cadastrar
 
-    Escolha: """))
+        Escolha: """))
         if escolha == 1:  
             login = input("Digite seu login: ")
-            senha = input("Digite sua senha: ")     
+            senha = input("Digite sua senha: ")
+            erros = []     
             if (login,senha) in administradores:
                 print("Logado com sucesso.")
                 logado = True
 
-            else:
+            elif (login,senha) not in administradores:
                 for x in range(len(administradores)):
                     if login not in administradores[x][0]:
-                        print("Usuario não encontrado")
-                        break
+                        erros.append("a")
+                    
+                for x in range(len(administradores)):
+                    if login in administradores[x][0] and senha not in administradores[x][1]:
+                        erros.append("b")
+                        
+            if "b" in erros:
+                print("Senha invalida.")
 
-                    elif senha not in administradores[x][1]:
-                        print("Senha invalida")
-                        break
+            elif "b" not in erros:
+                print("Usuario não encontrado.")
+                
+
 
 
         elif escolha == 2:
-            login = input("Digite o login de cadastro: ")
-            senha = input("Digite a senha de cadastro: ")
+            login2 = input("Digite o login de cadastro: ")
+            senha2 = input("Digite a senha de cadastro: ")
             confirma_senha = input("Repita a senha: ")
-            if senha == confirma_senha:
-                administradores.append((login,senha))
+            if senha2 == confirma_senha:
+                administradores.append((login2,senha2))
                 print("Cadastrado com sucesso")
             else:
                 print("Senhas divergentes")
@@ -43,5 +51,3 @@ while not logado:
             print("Você não escolheu uma opção valida")
     except:
         print("Ops... Ocorreu um erro... Tente novamente digitando um valor númerico...")
-
-
